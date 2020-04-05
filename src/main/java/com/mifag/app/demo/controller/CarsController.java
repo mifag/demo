cb2b6c293eb22daf333e970f16f7bb3037df8f7a
mifag <mifag92@rambler.ru>
2020-04-05 17:35:40 +0300

    агсл

--- src/main/java/com/mifag/app/demo/controller/CarsController.java
+++ src/main/java/com/mifag/app/demo/controller/CarsController.java
@@ -39,15 +39,29 @@ public class CarsController {
         return ResponseEntity.ok(car);
     }
 
+    /**
+     * К нам в контроллёр приходит post-запрос по протоколу http. Запрос содержит в себе тело. Запрос попадает в метод
+     * addCarDataDto. (Этот метод создает новую запись в таблице cars.)
+     * тело http запроса попадает в объект cdd класса CarDataDto.
+     */
     @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, path = "/body")
     public ResponseEntity<CarDataDto> addCarDataDto(@RequestBody CarDataDto cdd) {
+        // Мы переходим из слоя Controller, отвечающего за общение по протоколу http, в слой Service, содержащий в
+        // себе бизнес-логику данного API. Мы вызываем метод addCar сервиса CarService, представленного переменной
+        // carSer. В метод addCar мы передаем объект cdd класса CarDataDto.
         CarDataDto dff = carSer.addCar(cdd);
+        // Полученный из метода addCar сервиса CarService, представленного объектом carSer, объект класса CarDataDto
+        // присвоен объекту dff класса CarDataDto. Объект dff ("обернутый" в класс ResponseEntity)
+        // отправлен обратно на клиент по протоколу HTTP в ответ на пришедший запрос.
         return ResponseEntity.ok(dff);
+        // метод addCarDataDto возвращает объект класса CarDataDto (который содержит в себе данные о созданной
+        //записи в таблице cars), "обернутый" в класс ResposeEntity
     }
 
     @PutMapping(produces = MediaType.APPLICATION_JSON_VALUE, path ="/body/{carId}")
     public ResponseEntity<Cars> update (@RequestBody CarDataDto sgt,
                                               @PathVariable(value = "carId") Long carIdToUpdate) {
+
         Optional<Cars> updCar = repcars.findById(carIdToUpdate);
         if (updCar.isPresent()) {
             Cars detcars = updCar.get();
--- src/main/java/com/mifag/app/demo/service/CarService.java
+++ src/main/java/com/mifag/app/demo/service/CarService.java
@@ -5,6 +5,7 @@ import com.mifag.app.demo.dto.TableThreeDto;
 import com.mifag.app.demo.entity.Cars;
 import com.mifag.app.demo.repository.CarsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
@@ -41,15 +42,37 @@ public class CarService {
         return tabData;
     }
 
+    /**
+     * Метод addCar содержит бизнес-логику API addCarDataDto, находящегося в контроллере CarsController.
+     * Этот метод добавляет запись в таблицу cars и возвращает объект класса CarDataDto, содержащий в себе
+     * информацию о добавленной в таблицу cars записи.
+     * dataDto - Данный объект класса CarDataDto содержит в себе данные объекта cdd, переданного из метода
+     * addCarDataDto класса (Контроллера) CarsController. Этот объект используется для заполнения Entity Cars данными.
+     */
     public CarDataDto addCar(CarDataDto dataDto) {
         Cars bodyCars = new Cars();
         bodyCars.setType(dataDto.getTyC());
         bodyCars.setModel(dataDto.getMoC());
         bodyCars.setSpeed(dataDto.getSpC());
         bodyCars.setPower(dataDto.getPoC());
+        // Здесь происходит вызов репозитория CarsRepository, представленного объектом repcars. Вызывается
+        // метод save репозитория CarsRepository, представленного объетом repcars. В метод save передается
+        // entity Cars представленная объектом bodyCars. Возвращаемое методом save значение содержит в себе
+        // информацию о добавленной (сохранённой) записи в таблицу cars и является объектом класса Cars (Entity).
+        // Возвращаемый методом save репозитория CarsRepository, представленного объектом repcars, объект приравнен
+        // к новому объекту createdCar класса Cars.
         Cars createdCar = repcars.save(bodyCars);
         Long nam = createdCar.getId();
-        dataDto.setId(nam);
-        return dataDto;
+        CarDataDto carto = new CarDataDto();
+        carto.setId(nam);
+        carto.setMoC(createdCar.getModel());
+        carto.setPoC(createdCar.getPower());
+        carto.setSpC(createdCar.getSpeed());
+        carto.setTyC(createdCar.getType());
+        // Возвращаемое значение метода addCar (являющееся объектом класса CarDataDto)
+        // передается обратно в метод addCarDataDto контроллёра CarsController в объект dff.
+        return carto;
+        //Метод addCar возвращает данные о добавленной записи в таблицу cars (объект carto).
     }
+
 }
