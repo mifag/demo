3ef89bc52a699470f8d04b81583a972208742015
mifag <mifag92@rambler.ru>
2020-07-08 20:09:38 +0300

    Refactoring.

--- src/main/java/com/mifag/app/demo/oldClasses/CarsRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mifag.app.demo.oldClasses;
-
-import com.mifag.app.demo.oldClasses.Cars;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CarsRepository extends CrudRepository<Cars, Long> {
-}
--- src/main/java/com/mifag/app/demo/oldClasses/TableTwoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mifag.app.demo.oldClasses;
-
-import com.mifag.app.demo.oldClasses.TableTwo;
-import org.springframework.data.repository.CrudRepository;
-
-public interface TableTwoRepository extends CrudRepository<TableTwo, Long> {
-}
